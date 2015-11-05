c053c1ea219a819c9c8143a99338f14ff36f377d
LUIS ARCE <luisarce5@gmail.com>
2015-11-05 18:56:19 -0500

    Initial Set Up of Rails App: user Model; UsersController; users views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+    @test = "Hello world"
+  end
+
+  def new
+    # @user = User.new
+  end
+
+end   # end of class
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # has_many :loans
+  # has_secure_password
+
+  # validates :name, presence: true
+
+end
