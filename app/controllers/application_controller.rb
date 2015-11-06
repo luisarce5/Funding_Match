799f0ba0c9b7a8bda5835c360f5996241b424c88
LUIS ARCE <luisarce5@gmail.com>
2015-11-06 14:33:03 -0500

    added SessionsController and corresponding views/ ; modified ApplicationController accordingly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+  def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        # or =
+        # if current_user is already a thing, then don't look for it
+        # that way you only find the current_user one time
+        # if current_user is not true, then do the command User.find
+    end
+    helper_method(:current_user)
+    #this calls the helper method on user when the action loads
+
+  # You cannot view any show page unless you are logged in
+    def authorize # we can call it anything
+      redirect_to '/login' unless current_user # if there is a current user then it will never redirect
+    end
+
+end # end of class
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new #loging form
+  end
+
+  def create
+    # user puts in email and password
+    # create a new session if they match
+
+    user = User.find_by(user_name: params[:user_name]) # do not worry about strong params because we are not putting it into database
+    # binding.pry
+    if user && user.authenticate(params[:password]) #authenticate is a bcrypt method
+      session[:user_id] = user.id
+      redirect_to("/")
+
+    else
+      redirect_to("/login")
+    end
+  end
+
+  def destroy # logout
+    session[:user_id] = nil
+    redirect_to('/')
+  end
+
+end
