230042f835383df2622fcebb04c24137b64b8280
LUIS ARCE <luisarce5@gmail.com>
2015-11-06 12:45:24 -0500

    added Delete user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,15 @@ class UsersController < ApplicationController
       end
     end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(
     :user_name,
