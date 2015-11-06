bd72ac9540c1cb7807ffcd123c5232977a9f3f39
LUIS ARCE <luisarce5@gmail.com>
2015-11-06 12:24:35 -0500

    added partial form (-form), edit and show under views/users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,32 +8,45 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    user = User.new(user_params)
-
-    if user.save
-      redirect_to('/')
-    else
-      redirect_to 'users/new'
-    end
-  end
-
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # redirect_to('/')
+      redirect_to @user
+    else
+      # redirect_to 'users/new'
+      render 'new'
+    end
+  end
+
+  def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
   def user_params
     params.require(:user).permit(
     :user_name,
     :company_name,
+    :user_description,
     :town,
     :borrower,
     :password,
